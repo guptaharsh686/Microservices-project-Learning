3e4c31836058f5ea9462c9bcfc66a304cb281e72
LAPTOP-RARL5FJK <guptaharsh686@gmail.com>
2024-05-12 16:13:53 +0530

    PlatformService : Add PlatformRepo for dependancy Injection.

--- PlatformService/Program.cs
+++ PlatformService/Program.cs
@@ -15,6 +15,7 @@ namespace PlatformService
             builder.Services.AddDbContext<AppDbContext>(opt =>
                 opt.UseInMemoryDatabase("InMem")
             );
+            builder.Services.AddScoped<IPlatformRepo,PlatformRepo>();
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
