6a0dee2c4c85de90fbdffcaa511a23f0087a3da0
LAPTOP-RARL5FJK <guptaharsh686@gmail.com>
2024-05-14 23:47:03 +0530

    CommandService : Configure automapper and create mapping profiles.

--- CommandsService/Program.cs
+++ CommandsService/Program.cs
@@ -8,7 +8,8 @@ namespace CommandsService
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-
+            //Allows us to inject automapper in our service
+            builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
