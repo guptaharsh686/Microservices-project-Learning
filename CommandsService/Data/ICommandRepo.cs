7a4f9dc9cf2c99f7efa117b00db7a8de3b5cf5a2
LAPTOP-RARL5FJK <guptaharsh686@gmail.com>
2024-05-14 23:35:30 +0530

    CommandService :  Create entity relation through fluent api and use repository pattern to define and implement command repository.

---
+++ CommandsService/Data/ICommandRepo.cs
@@ -0,0 +1,24 @@
+﻿using CommandsService.Models;
+
+namespace CommandsService.Data
+{
+    public interface ICommandRepo
+    {
+        bool SaveChanges();
+
+        //platforms
+        IEnumerable<Platform> GetPlatforms();
+
+        void CreatePlatform(Platform platform);
+
+        bool PlatformExists(int platformId);
+
+        //commands
+
+        IEnumerable<Command> GetCommandsForPlatform(int platformId);
+
+        Command GetCommand(int platformId,int commandId);
+
+        void CreateCommand(int platformId,Command command);
+    }
+}
