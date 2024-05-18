5ca5f83f68759777e7a0c200938223425030daa5
LAPTOP-RARL5FJK <guptaharsh686@gmail.com>
2024-05-18 12:41:42 +0530

    CommandService : Generate DTO to recive the published platform and create a mapping profile to map published platform to our own platform model.

--- CommandsService/Profiles/CommandsProfile.cs
+++ CommandsService/Profiles/CommandsProfile.cs
@@ -12,6 +12,10 @@ namespace CommandsService.Profiles
             CreateMap<Platform, PlatformReadDTO>();
             CreateMap<CommandCreateDTO, Command>();
             CreateMap<Command,CommandReadDTO>();
+
+            //Explicitely define which member to map to as source - dest have different property names
+            CreateMap<PlatformPublishDTO, Platform>()
+                .ForMember(dest => dest.ExternalID, opt => opt.MapFrom(src => src.Id));
         }
     }
 }
