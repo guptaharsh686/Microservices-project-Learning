aaa7d98694797aee401ad5bcf57e97f9bb369c17
LAPTOP-RARL5FJK <guptaharsh686@gmail.com>
2024-05-19 02:23:32 +0530

    CommandService : Setup GRPC client for reciving requests

--- CommandsService/Profiles/CommandsProfile.cs
+++ CommandsService/Profiles/CommandsProfile.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using CommandsService.DTOs;
 using CommandsService.Models;
+using PlatformService;
 
 namespace CommandsService.Profiles
 {
@@ -16,6 +17,14 @@ namespace CommandsService.Profiles
             //Explicitely define which member to map to as source - dest have different property names
             CreateMap<PlatformPublishDTO, Platform>()
                 .ForMember(dest => dest.ExternalID, opt => opt.MapFrom(src => src.Id));
+
+
+            CreateMap<GrpcPlatformModel, Platform>()
+                .ForMember(dest => dest.ExternalID, opt => opt.MapFrom(src => src.PlatformId))
+                .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name))
+                .ForMember(dest => dest.Commands, opt => opt.Ignore());
+
+
         }
     }
 }
--- CommandsService/Program.cs
+++ CommandsService/Program.cs
@@ -2,6 +2,7 @@
 using CommandsService.AsyncDataServices;
 using CommandsService.Data;
 using CommandsService.EventProcessing;
+using CommandsService.SyncDataServices.Grpc;
 using Microsoft.EntityFrameworkCore;
 
 namespace CommandsService
@@ -23,6 +24,7 @@ namespace CommandsService
             builder.Services.AddSingleton<IEventProcessor, EventProcessor>();
             builder.Services.AddScoped<ICommandRepo, CommandRepo>();
             builder.Services.AddHostedService<MessageBusSubscriber>();
+            builder.Services.AddScoped<IPlatformDataClient, PlatformDataClient>();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
